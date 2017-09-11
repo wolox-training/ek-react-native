3b4ddf0943970ea3c2ea560314a22c208886f23e
erikankodelia <erika.kodelia@wolox.com.ar>
2017-09-11 14:03:52 -0300

    PR fixed

--- ReactNativeTraining/App.js
+++ ReactNativeTraining/App.js
@@ -1,30 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FlatList, View } from 'react-native';
 import { List, ListItem } from 'react-native-elements';
 
 import styles from './App.styles';
 import { contacts } from './src/utils/constants';
 
-const App = () => (
-  <List containerStyle={styles.container}>
-    <FlatList
-      ItemSeparatorComponent={() => <View style={styles.greyLine} />}
-      data={contacts}
-      keyExtractor={item => item.id}
-      renderItem={({ item }) => (
-        <ListItem
-          hideChevron
-          roundAvatar
-          avatar={item.avatar}
-          title={item.username}
-          titleStyle={styles.name}
-          style={styles.listItem}
-          subtitle={item.message}
-          subtitleStyle={styles.message}
-        />
-      )}
+export default class App extends Component {
+  separatorComponent = () => <View style={styles.greyLine} />;
+  renderItem = item => (
+    <ListItem
+      hideChevron
+      roundAvatar
+      avatar={item.avatar}
+      title={item.username}
+      titleStyle={styles.name}
+      style={styles.listItem}
+      subtitle={item.message}
+      subtitleStyle={styles.message}
     />
-  </List>
-);
+  );
 
-export default App;
+  render() {
+    return (
+      <List containerStyle={styles.container}>
+        <FlatList
+          ItemSeparatorComponent={this.separatorComponent}
+          data={contacts}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => this.renderItem(item)}
+        />
+      </List>
+    );
+  }
+}
